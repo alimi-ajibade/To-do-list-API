a4ad0cdb5965e4a62bab6e360c807b898593e7ea
HelloSolo <davidalimi148@gmail.com>
2022-07-01 14:28:42 +0100

    added djoser

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('lists/', include('list.urls'))
+    # path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('lists/', include('list.urls')),
 ]
